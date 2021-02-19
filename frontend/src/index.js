efa87dcae79b4dc23298b70ca0acd1595178a86b
Kevin <pitzer.kevinj@gmail.com>
2021-02-19 10:37:52 -0500

    added csrf to frontend and backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import './index.css';
 import App from './App';
 import configureStore from './store';
@@ -9,6 +10,9 @@ import configureStore from './store';
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
