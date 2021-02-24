491308c7e00615d90c4c46430886e34eed31384a
Kevin <pitzer.kevinj@gmail.com>
2021-02-24 10:45:13 -0500

    link css for signup modal

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+import './SignupModal.css';
 
 function SignupForm() {
   const dispatch = useDispatch();
