1faabfc47b1a3f2ec58bd3f98c0bd54ef4f46d5f
Kevin <pitzer.kevinj@gmail.com>
2021-02-24 23:04:22 -0500

    more styling

--- frontend/src/components/RestaurantPage/ReservationForm.js
+++ frontend/src/components/RestaurantPage/ReservationForm.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './ReservationForm.css'
 
 export default function ReservationForm({ restaurant }) {
 
   return (
-    <>
+    <div className="res-form">
       <h2 className="res-form-title">
         Make a reservation
       </h2>
-    </>
+    </div>
   )
 }
