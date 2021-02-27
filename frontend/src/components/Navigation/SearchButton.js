b13b23cb78f9f937e9e3edb1f9615cc41835972d
Kevin <pitzer.kevinj@gmail.com>
2021-02-27 13:49:10 -0500

    tweak res form styling

--- frontend/src/components/Navigation/SearchButton.js
+++ frontend/src/components/Navigation/SearchButton.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import SearchComponent from '../SearchComponent';
+// import '../SearchComponent/SearchComponent.css'
 
 export default function SearchButton() {
-
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className="search-image">
-      <i className="fas fa-search"></i>
-    </div>
+    <>
+      <button 
+        className="search-image"
+        onClick={() => setShowModal(true)}
+      >
+        <i className="fas fa-search"></i>
+      </button>
+      {showModal && (
+        <Modal 
+          onClose={() => setShowModal(false)}
+          className="search-button-modal"
+        >
+          <SearchComponent />
+        </Modal>
+      )}
+    </>
   )
 }
