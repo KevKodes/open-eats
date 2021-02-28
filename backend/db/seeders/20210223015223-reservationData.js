c3e567bac9c257f54757a9e8741cfbce6a7c9314
Kevin <pitzer.kevinj@gmail.com>
2021-02-28 11:44:15 -0500

    seed more reservations

--- backend/db/seeders/20210223015223-reservationData.js
+++ backend/db/seeders/20210223015223-reservationData.js
@@ -5,11 +5,46 @@ module.exports = {
       return queryInterface.bulkInsert('Reservations', [
         {
           partySize: 2,
-          reservationDate: "2021-03-13",
+          reservationDate: new Date(2021, 03, 13),
           reservationTime: "7:00 pm",
           userId: 1,
           restaurantId: 1
-        }
+        },
+        {
+          partySize: 2,
+          reservationDate: new Date(2021, 03, 09),
+          reservationTime: "8:00 pm",
+          userId: 1,
+          restaurantId: 2
+        },
+        {
+          partySize: 7,
+          reservationDate: new Date(2021, 02, 17),
+          reservationTime: "5:30 pm",
+          userId: 1,
+          restaurantId: 3
+        },
+        {
+          partySize: 13,
+          reservationDate: new Date(2020, 12, 24),
+          reservationTime: "6:00 pm",
+          userId: 1,
+          restaurantId: 3
+        },
+        {
+          partySize: 4,
+          reservationDate: new Date(2021, 02, 17),
+          reservationTime: "1:00 pm",
+          userId: 1,
+          restaurantId: 3
+        },
+        {
+          partySize: 2,
+          reservationDate: new Date(2021, 04, 21),
+          reservationTime: "8:00 pm",
+          userId: 1,
+          restaurantId: 3
+        },
       ], {});
   },
 
