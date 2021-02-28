dfc12a29cc28bac811c81a943f8127aa8180dd6b
Kevin <pitzer.kevinj@gmail.com>
2021-02-28 17:49:15 -0500

    removed users hardcoded id

--- backend/db/seeders/20210218185247-demo-user.js
+++ backend/db/seeders/20210218185247-demo-user.js
@@ -6,21 +6,18 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        id: 1,
         email: 'demo@user.io',
         firstName: 'Demo',
         lastName: 'User',
         hashedPassword: bcrypt.hashSync('password'),
       },
       {
-        id: 2,
         email: faker.internet.email(),
         firstName: 'John',
         lastName: 'Smith',
         hashedPassword: bcrypt.hashSync(faker.internet.password()),
       },
       {
-        id: 3,
         email: faker.internet.email(),
         firstName: 'Jane',
         lastName: 'Doe',
