5d0b2b7e62e947f7dda47a26e2358b328929012e
Kevin <pitzer.kevinj@gmail.com>
2021-05-04 16:24:06 -0400

    default reviews elements added

--- frontend/src/components/RestaurantPage/Reviews.js
+++ frontend/src/components/RestaurantPage/Reviews.js
@@ -6,11 +6,13 @@ import './Reviews.css';
 
 export default function Reviews() {
   const reviews = useSelector(state => state.reviews?.restaurantReviews)
+  const sessionUser = useSelector(state => state.session?.user);
   const [overallRating, setOverallRating] = useState(0)
   const [overallAverage, setOverallAverage] = useState(0); //to set the star component
   const [foodRating, setFoodRating] = useState(0)
   const [serviceRating, setServiceRating] = useState(0)
   const [ambienceRating, setAmbienceRating] = useState(0)
+  const [errors, setErrors] = useState([])
   // console.log('reviews returned to the component: ', reviews)
 
   useEffect(() => {
@@ -42,7 +44,12 @@ export default function Reviews() {
   }, [reviews])
 
   const addReview = () => {
-    console.log('open the review modal')
+    if (!sessionUser) {
+      setErrors(['Please sign in to submit a review'])
+    } else {
+      console.log('open the review modal')
+
+    }
   }
 
   return (
@@ -50,49 +57,60 @@ export default function Reviews() {
       <div className="reviews-title">
         <h2>What people are saying</h2>
       </div>
-      <div className="reviews-header">
-        <h3>Overall ratings and reviews</h3>
-        <div className="reviews-overall">
-          <div className="reviews-overall-stars">
+      { reviews?.length ? (
+        <div className="reviews-header">
+          <h3>Overall ratings and reviews</h3>
+          <div className="reviews-overall">
+            <div className="reviews-overall-stars">
 
+            </div>
+            <div className="reviews-overall-val">
+              <StarRatings
+                rating={overallAverage}
+                starRatedColor="#DA3743"
+                numberOfStars={5}
+                starDimension="20px"
+                starSpacing="2px"
+                name='rating'
+              /> {overallRating} based on recent reviews
+            </div>
           </div>
-          <div className="reviews-overall-val">
-            <StarRatings
-              rating={overallAverage}
-              starRatedColor="#DA3743"
-              numberOfStars={5}
-              starDimension="20px"
-              starSpacing="2px"
-              name='rating'
-            /> {overallRating} based on recent reviews
+          <div className="reviews-breakdown">
+            <div className="reviews-breakdown-section">
+              <div>{foodRating}</div>
+              <p>Food</p>
+            </div>
+            <div className="reviews-breakdown-section service-rating">
+              <div>{serviceRating}</div>
+              <p>Service</p>
+            </div>
+            <div className="reviews-breakdown-section">
+              <div>{ambienceRating}</div>
+              <p>Ambience</p>
+            </div>
+          </div>
+          <div className="reviews-recommendation">
+            <i className="fas fa-volume-up"></i>
+            <p>Moderate noise</p>
+          </div>
+          <div className="reviews-recommendation">
+            <i className="far fa-thumbs-up fa-xs"></i>
+            <p><strong>95% of people</strong> would recommend it to a friend</p>
           </div>
         </div>
-        <div className="reviews-breakdown">
-          <div className="reviews-breakdown-section">
-            <div>{foodRating}</div>
-            <p>Food</p>
+      ) : (
+        <h3 className="review-header-bottom">Be the first to review this restaurant!</h3>
+      )}
+        <div className="review-header-bottom">
+          <ul className='res-form-errors'>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <div className="add-review">
+            <button onClick={addReview}>Add a Review</button>
           </div>
-          <div className="reviews-breakdown-section service-rating">
-            <div>{serviceRating}</div>
-            <p>Service</p>
-          </div>
-          <div className="reviews-breakdown-section">
-            <div>{ambienceRating}</div>
-            <p>Ambience</p>
-          </div>
         </div>
-        <div className="reviews-recommendation">
-          <i className="fas fa-volume-up"></i>
-          <p>Moderate noise</p>
-        </div>
-        <div className="reviews-recommendation">
-          <i className="far fa-thumbs-up fa-xs"></i>
-          <p><strong>95% of people</strong> would recommend it to a friend</p>
-        </div>
-        <div className="add-review">
-          <button onClick={addReview}>Create a Review</button>
-        </div>
-      </div>
       <div className="reviews-body">
         {reviews && reviews.map((review, idx) => (
           <ReviewBlock key={idx} review={review} />
